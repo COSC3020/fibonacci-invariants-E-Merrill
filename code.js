766adbd0b52b2fd139bd466d2082004c5f431a6f
E-Merrill <emerril3@uwyo.edu>
2025-02-19 12:32:06 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,7 @@
-function fib(n) {
-    return n;
+function fib(n, fibarr=[0, 1]) {
+    if(fibarr.length > n){
+        return fibarr.slice(0, n + 1);
+    }
+    fibarr[fibarr.length] = fibarr[fibarr.length - 1] + fibarr[fibarr.length - 2];
+    return fib(n, fibarr)
 }
